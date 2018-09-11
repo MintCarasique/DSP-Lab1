37fe51324ae8b76974b41b42774cf257e96cb978
MintCarasique <mykew98@gmail.com>
2018-09-11 11:21:48 +0300

    Added harmonic signal drawing

---
+++ CSPLab1.SignalLib/Harmonic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPLab1.SignalLib
+{
+    public static class Harmonic 
+    {
+
+        static Harmonic()
+        {
+            
+        }
+        public static double Calc(int a, int N, int n, double fi, int f, int k)
+        {
+
+            return a * Math.Sin(2 * Math.PI * f * n / N + fi);
+        }
+    }
+}
--- CSPLab1/Form1.cs
+++ CSPLab1/Form1.cs
@@ -9,17 +9,33 @@ using System.Threading.Tasks;
 using OxyPlot;
 using OxyPlot.Series;
 using System.Windows.Forms;
+using CSPLab1.SignalLib;
 
 namespace CSPLab1
 {
     public partial class Form1 : Form
     {
+        private int a = 10;
+
+        private int n = 511;
+        private double fi = Math.PI/2;
+        private int f = 2;
+        private int k = 10;
+        private int N = 512;
+
         public Form1()
         {
             InitializeComponent();
             var harmonicModel = new PlotModel(){Title =  "Harmonic signal"};
-            harmonicModel.Series.Add(new FunctionSeries(Math.Sin, 0, 10, 0.5, "sin(x)"));
+            Func<double, double> HarmonicFunc = Calc;
+            
+            harmonicModel.Series.Add(new FunctionSeries(HarmonicFunc, 0, n, 0.5, "Harmonic"));
             this.plotView.Model = harmonicModel;
+        }
+
+        public double Calc(double n)
+        {
+            return a * Math.Sin(2 * Math.PI * f * n / N + fi);
         }
     }
 }
